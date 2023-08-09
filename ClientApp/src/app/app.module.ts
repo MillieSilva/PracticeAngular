6725a36023a41828e09f1ca74c17d815471b8ac8
Millie <milliem@tuta.io>
2023-08-09 11:03:42 +0100

    🌸 Implemented post previews, stylized the home page and added some images

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -18,16 +19,17 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     CounterComponent,
     FetchDataComponent
   ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
-  ],
+    imports: [
+        BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
+        HttpClientModule,
+        FormsModule,
+        RouterModule.forRoot([
+            {path: '', component: HomeComponent, pathMatch: 'full'},
+            {path: 'counter', component: CounterComponent},
+            {path: 'fetch-data', component: FetchDataComponent},
+        ]),
+        NgOptimizedImage
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  posts: Post[] = [
+    {id: 1, name: "Test post", author: "Millie"},
+    {id: 2, name: "A little apricot grows", author: "Millie"},
+    {id: 3, name: "Lettuces become flowers", author: "Millie"},
+    {id: 4, name: "Growing Coriander", author: "Millie"},
+    {id: 5, name: "Another test post", author: "Millie"},
+  ]
+
+  ngOnInit() {
+    // TODO: Initialize with fetched data
+  }
+}
+
+interface Post {
+  id: number,
+  name: string,
+  author: string
 }
